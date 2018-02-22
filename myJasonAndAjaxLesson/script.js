15765b92658c5895e87243c227442952dc307cf6
Paweł <pkurpinski@wp.pl>
2018-02-22 12:41:45 +0100

    22.02 adding objects

--- myJasonAndAjaxLesson/script.js
+++ myJasonAndAjaxLesson/script.js
@@ -1,1 +1,18 @@
-alert("alala")
+var myCat = {
+    "name":"Miałczek",
+    "species":"cat",
+    "favFood": "tuna"
+}
+
+var myFavColours = [
+    {
+        "name":"Miałczek",
+        "species":"cat",
+        "favFood": "tuna"
+    },
+    {
+        "name":"Miki",
+        "species":"cat",
+        "favFood": "tuna"
+    }
+]
