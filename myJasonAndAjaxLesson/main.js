514302784206652623de07372d0e0c81e9b563b5
Paweł <pkurpinski@wp.pl>
2018-02-23 18:24:00 +0100

    23.02.18 Ajax c.d.

--- myJasonAndAjaxLesson/main.js
+++ myJasonAndAjaxLesson/main.js
@@ -1,5 +1,6 @@
 // przeglądarka ma wbudowane narzędzie XMLHttpRequest pozwala na połączenie z url'em jaki podamy oraz na odebranie lub przesłanie danych; Teraz tworzymy instancję tego narżędzia
 
+var myDiv = document.getElementById('animal-info');
 
 var myButton = document.getElementById('btn');
 myButton.addEventListener("click", function(){
@@ -9,10 +10,12 @@ myButton.addEventListener("click", function(){
         // onload mówi nam co mamy zrobić z pobranymi danymi; W ciele można zrobić co chcemy np. wyświetlić pobraną zawartość jako czytelny obiekt
         // console.log(ourRequest.responseText);
         var ourData = JSON.parse(ourRequest.responseText);
-        console.log(ourData[0]);
+        renderHTML(ourData);
         // ta linijka nie może działać bez zapisanego powyżej JSON.parse bo bez niego przeglądarka nie wie że to będzie obiekt a nie duży string
     };
     ourRequest.send();
+});
 
-    // var myDiv = document.getElementById('animal-info').innerHTML = (ourData);
-})
+function renderHTML(){
+    myDiv.insertAdjacentHTML('beforend', 'test123')
+}
