d8cc034da0eec57cff3febe621f731b9ef08b3be
Paweł <pkurpinski@wp.pl>
2018-03-13 13:58:40 +0100

    save!

--- myVueFirstSteps/app.js
+++ myVueFirstSteps/app.js
@@ -1,6 +1,12 @@
 new Vue({
   el:'#vue-app',
   data:{
-    name: "Paweł",
+    name: 'Paweł',
+    job: 'Developer'
+  },
+  methods:{
+    greet: function(time){
+      return 'Good ' + time;
+    }
   }
 });
