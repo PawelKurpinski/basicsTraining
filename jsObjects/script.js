c24270aee310a4116f48829a11fbe267d053d328
Paweł <pkurpinski@wp.pl>
2018-03-14 14:05:23 +0100

    14.03 zapis

--- jsObjects/script.js
+++ jsObjects/script.js
@@ -14,14 +14,11 @@ var person = {
 }
 
 function Animal(getName, getSound){
-	this.name = getName,
-    this.sound = getSound ()
-        { console.log(getSound)
-    }
+	this.name = getName
 }
 
 var dog = new Animal("Reksio","wow wow");
-	dog.sound();
+
 
 function Person(name){
     this.name = name;
@@ -30,7 +27,7 @@ function Person(name){
     }
 }
 elie = new Person("Elie");
-elie.sayHi();
+
 
 // inna opcja to poniższy zapis
 
@@ -62,5 +59,51 @@ Vehicle.prototype.turnOff = function(){
 Vehicle.prototype.honk = function(){
     if (this.isRunning){
     return "beep!";
+    }
+}
+
+//-------------------------------------------------------------------------------------------------------------------- 
+// TWORZENIE OBIEKTU NA BAZIE OBIEKTU WBUDOWANEGO
+myNewObject = new Object;
+// dodaję właściwość
+console.log('fist stage: '+ myNewObject.info); 
+
+myNewObject.info = 'Jestem Pawła nowym obiektem';
+
+// dodaję metodę
+function myFunc(){
+    alert("ale zabawa");
+};
+myNewObject.showInfo = myFunc;
+
+// dodaję drugą metodę
+function myFuncSecond(){
+    alert(this.info);
+};
+myNewObject.showInfoSecond = myFuncSecond;
+
+// -------------------------------------------------------------------------------
+// UWAGA! ładniej można to napisać za pomocą FUNKCJI ANONIMOWEJ! 
+
+myNewObject.showInfoThird = function() {
+    alert('we have made it!');
+}
+
+// poniżej znajduje się wywołanie funkcji - UWAGA! samo "showInfo" nic by nie dało
+// ponieważ odwoływałoby się do obiektu nadrzędnego Window a nie mojego myNewObject.
+
+myNewObject.showInfo();  
+myNewObject.showInfoSecond();
+myNewObject.showInfoThird();
+
+console.log('last stage: '+ myNewObject.info); 
+
+// --------------------------------------------------------------------------------------------------------------------
+// TWORZENIE OBIEKTU za pomocną WYRAŻENIA FUNKCYJNEGO inaczej poprzez użycie FUNKCJI KONSTRUKTORA OBIEKTU
+
+function myNewObjectType(){
+    this.info = 'Też jestem obiektem';
+    this.showMeInfo = function(){
+        alert(this.info); /* wyświetlam zapisane powyżej info */
     }
 }
